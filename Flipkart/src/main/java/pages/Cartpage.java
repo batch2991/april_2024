8367f49066c6d5c8b4628979254535b6230f920f
venkat <venkatgn.gutta@gmail.com>
2024-06-02 08:21:17 +0530

    CartPage,HomePage is updated with new methods

--- Flipkart/src/main/java/pages/Cartpage.java
+++ Flipkart/src/main/java/pages/Cartpage.java
@@ -4,8 +4,12 @@ public class Cartpage
 {
 	public void addProductTotheCart()
 	{
+		System.out.println("code to add product to cart");
 		
-		
+	}
+	public void validateProducts()
+	{
+		System.out.println("code to check products in cart");
 	}
 	
 
--- Flipkart/src/main/java/pages/HomePage.java
+++ Flipkart/src/main/java/pages/HomePage.java
@@ -9,8 +9,13 @@ public class HomePage extends BasePage
 {
 	By searchinput=By.name("q");
 	
+	//To search for a given product type in the homepage
 	public void search(String prodtype)
 	{
 		driver.findElement(searchinput).sendKeys(prodtype,Keys.ENTER);
+	}
+	public void getLinks()
+	{
+		
 	}
 }
